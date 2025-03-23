1221926cfb6d6e0ba644aa2caf112933dd001c5b
uchechi14 <uchechinnorom9@gmail.com>
2025-03-23 20:51:40 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import LandingPage from "./components/LandingPage";
-import Servicess from "./servicess/Page";
+// import Servicess from "./servicess/Page";
 
 export default function Page() {
   return (
     <div>
       <LandingPage />
-      <Servicess />
+      {/* <Servicess /> */}
     </div>
   );
 }
